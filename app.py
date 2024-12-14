63bd042caff999b290dbbce2fd58ca0a3dc02400
emanuel7oliveira <emanuel385@protonmail.com>
2024-12-13 22:04:14 -0300

    A login menu working, but need some fix

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, session, request
 
 app = Flask(__name__, template_folder='templates', static_folder='templates/css')
+app.secret_key = "123456"
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    username = ''
+    if 'username' in session:
+        username = session['username']
+    return render_template('homepage.html', username=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and request.form['username'] != '':
+        session['username'] = request.form['username']
+        return redirect(url_for('homepage'))
+    return render_template('login.html')
+    
+@app.route('/logout')
+def logout():
+    session.pop('username', '')
+    return redirect(url_for('homepage'))
 
 @app.route('/gooogle')
 def google():
